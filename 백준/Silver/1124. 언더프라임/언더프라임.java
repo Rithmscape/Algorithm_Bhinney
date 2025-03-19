9bb3a883cb888084ff6a2c283d108420dfa9a08e
Bhinney <107696895+Bhinney@users.noreply.github.com>
2025-03-19 15:56:22 +0900

    [Silver I] Title: 언더프라임, Time: 200 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.java
+++ 백준/Silver/1124. 언더프라임/언더프라임.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,38 +7,38 @@ public class Main {
 		int b = sc.nextInt();
 		sc.close();
 
-		solution(a, b);
+		System.out.println(solution(a, b));
 	}
 
-	private static void solution(int a, int b) {
+	private static int solution(int a, int b) {
 		int ans = 0;
 		for (int i = a; i <= b; i++) {
-			int factors = factors(i).size();
-			if (isPrime(factors)) ans++;
+			if (isPrime(factors(i))) ans++;
 		}
 
-		System.out.println(ans);
+		return ans;
 	}
 
 	// 소인수 목록을 반환하는 메서드
-	private static List<Integer> factors(int n) {
-		List<Integer> result = new ArrayList<>();
+	private static int factors(int n) {
+
+		int cnt = 0;
 
 		while (n % 2 == 0) {
-			result.add(2);
+			cnt++;
 			n /= 2;
 		}
 
 		for (int i = 3; i <= Math.sqrt(n); i += 2) {
 			while (n % i == 0) {
-				result.add(i);
+				cnt++;
 				n /= i;
 			}
 		}
 
-		if (n > 1) result.add(n);
+		if (n > 1) cnt++;
 
-		return result;
+		return cnt;
 	}
 
 	// 소수 판별
