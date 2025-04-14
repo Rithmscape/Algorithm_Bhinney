9fac303ff66b9500941b7ea7fc14116c9d0e7fae
Bhinney <107696895+Bhinney@users.noreply.github.com>
2025-04-14 10:14:53 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		input();
+		solution();
+		System.out.println(sb.toString());
+	}
+	private static int N,M;
+	private static int[] arr;
+	private static int[] res;
+	private static boolean[] visited;
+	private static StringBuilder sb;
+
+	private static void solution() {
+		arr = IntStream.rangeClosed(1, N).toArray();
+		res = new int[M];
+		visited = new boolean[N];
+		sb = new StringBuilder();
+		perm(0, 0);
+	}
+
+	private static void perm(int idx, int depth) {
+		if (depth == M) {
+			sb.append(
+				Arrays.stream(res).mapToObj(String::valueOf).collect(Collectors.joining(" "))
+			).append("\n");
+			return;
+		}
+
+		for (int i = idx; i < N; i++) {
+			if (visited[i]) continue;
+
+			visited[i] = true;
+			res[depth] = arr[i];
+			perm(i + 1, depth + 1);
+			visited[i] = false;
+		}
+	}
+
+	private static void input() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		N = nm[0];
+		M = nm[1];
+	}
+}
