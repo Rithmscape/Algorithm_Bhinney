ef4a11569e3adf0ffea1e006c67bde97a18cecbc
Bhinney <107696895+Bhinney@users.noreply.github.com>
2025-05-16 14:05:35 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 104 ms, Memory: 15440 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
@@ -1,22 +1,16 @@
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readln().toInt()
     val ans = StringBuilder()
-    var cnt = 1
 
-    for (i in 1 ..  n) {
-        repeat(n - i) {ans.append(" ")}
-        repeat(cnt) {ans.append("*")}
-        ans.append("\n")
-        cnt += 2
-    }
+    for (i in 1 ..  n)
+        ans.append(" ".repeat(n - i))
+            .append("*".repeat(2 * i - 1))
+            .append("\n")
 
-    cnt -= 4
-    for (i in n - 1 downTo  1) {
-        repeat(n - i) {ans.append(" ")}
-        repeat(cnt) {ans.append("*")}
-        ans.append("\n")
-        cnt -= 2
-    }
+    for (i in n - 1 downTo  1)
+        ans.append(" ".repeat(n - i))
+            .append("*".repeat(2 * i - 1))
+            .append("\n")
 
     println(ans.toString().trimEnd())
 }
