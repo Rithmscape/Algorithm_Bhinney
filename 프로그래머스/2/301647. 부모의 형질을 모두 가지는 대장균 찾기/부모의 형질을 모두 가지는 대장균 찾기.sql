f308c109135acc528efb038c321de4c76b50b221
Bhinney <107696895+Bhinney@users.noreply.github.com>
2025-07-17 10:39:15 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,7 @@
+select c.id, c.genotype, p.genotype as parent_genotype
+from ecoli_data as c
+inner join ecoli_data as p
+on c.parent_id = p.id
+where c.genotype & p.genotype = p.genotype
+order by id
+ 
