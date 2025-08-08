697f72148a6ac0be4563d71223afccdf580731ff
Bhinney <107696895+Bhinney@users.noreply.github.com>
2025-08-08 15:50:55 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 188 ms, Memory: 17916 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -2,67 +2,32 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		new Solution().solve();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		StringBuilder sb = new StringBuilder();
+		sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+		recursion(n, 0, sb);
+		System.out.println(sb.toString());
 	}
 
-	private static class Solution {
-		private static final String BEFORE = "____";
-		private static final String QUESTION = "\"재귀함수가 뭔가요?\"";
-		private static final String INTRO1 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
-		private static final String INTRO2 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
-		private static final String INTRO3 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
-		private static final String ANSWER = "\"재귀함수는 자기 자신을 호출하는 함수라네\"";
-		private static final String FINISH = "라고 답변하였지.";
-		private static final String BR = "\n";
+	private static void recursion(int n, int cnt, StringBuilder sb) {
+		String blank = "____".repeat(cnt);
+		sb.append(blank).append("\"재귀함수가 뭔가요?\"\n");
 
-		private int n;
-		private StringBuilder answer;
+		if (n == cnt)
+			sb.append(blank).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+		else {
+			sb.append(blank)
+				.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n")
+				.append(blank)
+				.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n")
+				.append(blank)
+				.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
 
-		public void solve() {
-			input();
-			solution();
-			output();
+			recursion(n, cnt + 1, sb);
 		}
 
-		private void solution() {
-			answer = new StringBuilder();
-			answer
-				.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
-
-			recursion(0);
-		}
-
-		private void recursion(int cnt) {
-			String before = BEFORE.repeat(cnt);
-
-			if (cnt == n) {
-				answer
-					.append(before).append(QUESTION).append(BR)
-					.append(before).append(ANSWER).append(BR)
-					.append(before).append(FINISH).append(BR);
-
-				return;
-			}
-
-			String intro = before + INTRO1 + before + INTRO2 + before + INTRO3;
-
-			answer
-				.append(before).append(QUESTION).append(BR)
-				.append(intro).append(BR);
-
-			recursion(cnt + 1);
-
-			answer.append(before).append(FINISH).append(BR);
-		}
-
-		private void input() {
-			try (Scanner sc = new Scanner(System.in)) {
-				n = sc.nextInt();
-			}
-		}
-
-		private void output() {
-			System.out.println(answer);
-		}
+		sb.append(blank).append("라고 답변하였지.\n");
 	}
 }
